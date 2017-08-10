d1fd22e19c3faafe77251d32d7ca41ed8d1f1a7e
Marek Suscak <marek.suscak@gmail.com>
2017-08-10 23:30:54 +0200

    Implement obfuscate.

--- src/obfuscate.js
+++ src/obfuscate.js
@@ -1,3 +1,23 @@
+const compress = {
+  '.': (length) => String(length),
+  '-': (length) => String.fromCharCode(64 + length)
+}
+
+/**
+ * Obfuscates a given text by running a modified run-length
+ * encoding compression algorithm.
+ *
+ * @param {string} text The string to obfuscate.
+ */
 module.exports = function obfuscate(text) {
-  return text
+  if (!text || typeof text !== 'string') {
+    return ''
+  }
+
+  return text.replace(/-+|\.+/g, (match, offset, string) => {
+    const char = match[0]
+    const length = match.length
+
+    return compress[char](length)
+  })
 }
